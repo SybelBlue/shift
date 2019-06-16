fd86ca044038b10d572e21f792da1e5a5c5e74c2
SybelBlue <lbc.dna@gmail.com>
2019-06-15 22:50:56 -0500

    Username making complete.

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ io.sockets.on('connection', newConnection);
 
 function newConnection(socket) {
   console.log('new connection: ' + socket.id);
-  socket.emit('username', Date.now());
+  socket.emit('username', makeUsername());
   socket.broadcast.emit('new-player', {socketId: socket.id});
   var clients = Object.keys(io.sockets.sockets);
   io.sockets.emit('players', clients);
@@ -29,4 +29,26 @@ function newConnection(socket) {
     console.log(data);
     socket.broadcast.emit('draw', data);
   }
+}
+
+var takenNames = [];
+function makeUsername() {
+  function getRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length) % arr.length];
+  }
+  var adjs = ['ajax', 'automated', 'agile', 'block-chain', 'crowd-sourced',
+  'machiine', 'micro', 'nano', 'dynamic', 'cloud-based', 'decentralized',
+  'networked', 'crowd-sourced', 'functional', 'algorithmic', 'client-side',
+  'uncommented', 'refactored', 'lite', 'propreitary', 'end-to-end', 'buggy',
+  'assembled', 'bit-shifted', 'binary', 'remote', 'big-data', 'innovative']
+  var nouns = ['giraffe', 'pillow', 'penguin', 'poetry', 'serif',
+  'bison', 'dolphin', 'eagle', 'code-monkey', 'shepherd', 'duck', 'bunny',
+  'wolf', 'turkey', 'lion', 'piglet', 'snek', 'wrench', 'chicken', 'bolt',
+  'tabby', 'corgi', 'hammer', 'pencil', 'dormouse', 'pelican', 'coffee']
+  var name = getRandom(adjs) + ' ' + getRandom(nouns);
+  while (takenNames.includes(name)) {
+    name = getRandom(adjs) + ' ' + getRandom(nouns);
+  }
+  takenNames.push(name);
+  return name;
 }
